bcae34d73d363349ba9ddc521da613328367b6b4
Giancarlo Illescas <giancarlo.hoyos@gmail.com>
2023-11-09 22:48:39 -0500

    Change styles and general config

--- src/components/suggestions/suggestionsList/suggestionItem/suggestionCommentCounter.tsx
+++ src/components/suggestions/suggestionsList/suggestionItem/suggestionCommentCounter.tsx
@@ -10,7 +10,7 @@ const SuggestionsCommentCounter = ({ commentCount }: Props) => {
   return (
     <div className='suggestion-comment-counter_container'>
       {/* // TODO: is alt property required here? */}
-      <img src={CommentImage} alt="Comment counter" />
+      <img src={CommentImage} alt="Comment counter" aria-hidden='true' />
       <span className='suggestion-comment-counter_text'>{commentCount}</span>
     </div>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
-import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "src//assets/styles/index.scss";`
-      }
-    }
-  },
+  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/tests/setup.ts',
   }
 })
+
+// css: {
+//   preprocessorOptions: {
+//     scss: {
+//       additionalData: `@import "src//assets/styles/index.scss";`
+//     }
+//   }
+// },
